a5efffe5ca87d7f4dd759da10e43a85dce6842d2
Vishal Sinha <sinhav2@uci.edu>
2026-04-19 03:25:23 -0700

    remove repeated space struct from Piece.h

--- src/Piece.h
+++ src/Piece.h
@@ -60,16 +60,7 @@ typedef struct{
 
 } Piece;
 
-//this space struct represents a single coordinate on the board
-typedef struct{
-	Piece * piece; //pointer to piece
-	Rank rankNum;// row coordinate
-	File fileNum; //column coordinate
-	Color color; // color of square
-
-}Space;
-
-PieceType getPieceType(Piece p);
+PieceType getPieceType(Piece *p);
 Color getPieceColor(Piece p);
 char* getMoveSet(Piece *p);
 Piece storePiece(Piece p);
