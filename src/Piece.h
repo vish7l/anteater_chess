f995ebd378500b9628ed18c6679320885079185a
Vishal Sinha <sinhav2@uci.edu>
2026-04-19 19:53:38 -0700

    remove space struct from Piece.h, fix function signatures to use pointers and remove moveSet function or field declarations

--- src/Piece.h
+++ src/Piece.h
@@ -7,47 +7,16 @@
 typedef enum{
     Black,
     White
-
 }Color;
 
 //defines the all 7 of the piecetypes
-typedef enum{
-    PAWN,
-    ANTEATER,
-    KING,
-    QUEEN,
-    ROOK,
-    BISHOP,
-    KNIGHT
-
-
-}PieceType;
+typedef enum{PAWN, ANTEATER, KING, QUEEN, ROOK, BISHOP, KNIGHT} PieceType;
 
 //defines the 8 rows
-typedef enum{
-    R1,
-    R2,
-    R3,
-    R4,
-    R5,
-    R6,
-    R7,
-    R8
-}Rank;
+typedef enum{R1, R2, R3, R4, R5, R6, R7, R8} Rank;
 
 //defines the 10 columns
-typedef enum{
-    A,
-    B,
-    C,
-    D,
-    E,
-    F,
-    G,
-    H,
-    I,
-    J
-}File;
+typedef enum{A, B, C, D, E, F, G, H, I, J} File;
 
 //Next the structs
 
@@ -55,14 +24,10 @@ typedef enum{
 typedef struct{
     Color color;
     PieceType type;
-    //pointer to the function that calculates the legal moves
-    char* moveSet;
-
 } Piece;
 
 PieceType getPieceType(Piece *p);
-Color getPieceColor(Piece p);
-char* getMoveSet(Piece *p);
+Color getPieceColor(Piece *p);
 Piece storePiece(Piece p);
 
 #endif
